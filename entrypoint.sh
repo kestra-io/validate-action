f9dd7294475f339c417ea43b76a9016b1219ce3f
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-11-12 08:27:35 +0100

    feat: add usage events
    close kestra-io/kestra-ee#2076

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ if [[ $3 ]]; then
   if [[ ${6} ]]; then
     apiToken="--api-token=${6}"
   fi
-  
+
   # Check if there is a tenant id
   tenant=""
   if [[ $7 ]]; then
@@ -22,4 +22,23 @@ else
   location="--local"
 fi
 
+# Track usage
+CONFIG=$(curl -s https://api.kestra.io/v1/config)
+PH_TOKEN=$(echo "$CONFIG" | sed -E  's/.*"token":"([^"]+)".*/\1/')
+PH_API_HOST=$(echo "$CONFIG" | sed -E  's/.*"apiHost":"([^"]+)".*/\1/')
+PH_ID=$(echo "$CONFIG" | sed -E  's/.*"id":"([^"]+)".*/\1/')
+
+curl -s -L -H "Content-Type: application/json" -d '{
+    "api_key": "'"$PH_TOKEN"'",
+    "event": "Github Actions",
+    "distinct_id": "'"$PH_ID"'",
+    "properties": {
+        "gha": {
+            "name": "validate-action",
+            "resource": "'"$2"'"
+        }
+    }
+}' "$PH_API_HOST"capture/ > /dev/null
+
+# Run it
 /app/kestra "$2" validate "$1" $location $apiToken $tenant
